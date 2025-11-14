a1b2f0bc68b2e475195b0acf9c4af7b94144b9cd
izaiascrs <40672426+izaiascrs@users.noreply.github.com>
2025-11-14 00:03:32 -0300

    Change contract type from MULTUP to MULTDOWN

--- src/index.ts
+++ src/index.ts
@@ -205,11 +205,11 @@ const task = schedule('56 * * * * *', async () => {
       // if(!lastZeroLagData) continue;
       // if (lastZeroLagData.trendChange === false) continue;
     
-      let contractType: NonNullable<BuyContractRequest["parameters"]>["contract_type"] = "MULTUP";
+      let contractType: NonNullable<BuyContractRequest["parameters"]>["contract_type"] = "MULTDOWN";
     
       // bearish trend
       if(validCandleUpperDistance) {
-        contractType = "MULTDOWN"
+        contractType = "MULTUP"
       }
 
       // if(multipliersDirectionMap.get(symbol) === false) {
